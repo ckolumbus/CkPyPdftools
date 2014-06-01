a654f7d3fdc182a82310dce0fcdd97e6cf59c011
CKolumbus <ckolumbus@ac-drexler.de>
2014-06-01 20:20:57 +0200

    added tox support with setup.py (search only)

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='CkPyPdfTools',
+      author="Chris Drexler",
+      author_email="ckolumbus@ac-drexler.de",
+      url="https://github.com/ckolumbus/CkPyPdftools",
+      version='1.0',
+      py_modules=['ckFileIndexSearch'],
+      package_dir = {'': 'src'}
+     )
